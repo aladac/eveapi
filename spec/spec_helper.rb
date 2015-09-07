96023e967a93dae76c39108979ede1a072246a99
Adam Ladachowski <adam@saiden.pl>
2015-09-07 10:19:17 +0200

    Start simplecov only when no Code Climate token present

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
-require 'simplecov'
-SimpleCov.start
+unless ENV['CODECLIMATE_REPO_TOKEN']
+  require 'simplecov'
+  SimpleCov.start
+end
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 require 'rspec'
