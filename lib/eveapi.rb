1a35c732fd3dfcada323622079d619faeba99202
Adam Ladachowski <adam@saiden.pl>
2015-09-13 00:14:45 +0200

    reduced working methods to only the ones actually defined

--- lib/eveapi.rb
+++ lib/eveapi.rb
@@ -18,28 +18,11 @@ module EVEApi
   CREST_ENDPOINT = 'https://public-crest.eveonline.com/'
   API_ENDPOINT = 'https://api.eveonline.com'
   WORKING_METHODS = [
-    :account_api_key_info,
-    :account_account_status,
-    :account_characters,
-    :server_server_status,
-    :char_contracts,
-    :char_wallet_transactions,
-    :char_wallet_journal,
-    :char_upcoming_calendar_events,
-    :char_standings,
-    :char_skill_queue,
-    :char_skill_in_training,
-    :char_research,
-    :char_notifications,
-    :char_medals,
-    :char_market_orders,
-    :char_mail_messages,
-    :char_mailing_lists,
-    :char_industry_jobs,
-    :char_contact_notifications,
-    :char_contact_list,
-    :char_character_sheet,
-    :char_asset_list,
-    :char_account_balance
+    :account_status,
+    :server_status,
+    :characters,
+    :api_methods,
+    :key_info,
+    :call_list
   ]
 end
